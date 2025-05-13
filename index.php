14176feb6869bf62333d8f6f45cb1d2daf77f70a
lsaspis <lsaspis@gmail.com>
2025-05-13 01:52:54 -0300

    nose que paso aca

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php
 
-    echo 'voy a probar comitear esto';
-
- ?>
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 
@@ -12,6 +8,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRSeguros</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
     <script>
         document.documentElement.classList.remove('no-js');
         document.documentElement.classList.add('js');
@@ -27,6 +25,34 @@
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
     <link rel="manifest" href="site.webmanifest">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+     <!-- Favicons -->
+    <link href="img/cyrlogo.jpg" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: OnePage
+  * Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
     <style>
         .whatsapp-button {
             position: fixed; /* Mantiene el botón fijo en la pantalla */
@@ -46,12 +72,36 @@
         .whatsapp-button:hover {
             background-color: #128C7E; /* Color de fondo al pasar el mouse */
         }
+
+         /* .whatsapp-button {
+        position: fixed;
+        right: 15px;
+        bottom: 70px;
+        background-color: #25D366;
+        color: white;
+        padding: 10px 15px;
+        border: none;
+        border-radius: 5px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+        text-decoration: none;
+        font-size: 16px;
+        z-index: 100;
+        width: 40px;
+        height: 40px;
+    }
+
+    .whatsapp-button:hover {
+        background-color: #128C7E;
+    } */
+        
     </style>
 </head>
 
 
 <body id="top">
 <button class="whatsapp-button" onclick="window.open('https://wa.me/1155820489?text=Estoy%20interesado%20en%20sus%20seguros.', '_blank')"><i class="fab fa-whatsapp"></i></button>
+ <!-- <a href="https://wa.me/1155820489?text=Estoy%20interesad@%20en%20sus%20seguros" target="_blank"
+        class="whatsapp-button"><i class="fab fa-whatsapp"></i></a> -->
     <!-- preloader
     ================================================== -->
     <div id="preloader">
